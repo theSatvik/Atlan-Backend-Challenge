8f5162160fea0c264d68928d0fba757587e161c2
theSatvik <satvikshrivas26@gmail.com>
2021-07-13 23:43:46 +0530

    Added export and download feature of csv

--- db.js
+++ db.js
@@ -1,10 +1,10 @@
 const Pool = require('pg').Pool;
 
 const pool = new Pool({
-    user:"postgres",
-    password:"Satvik26#",
-    port:5432,
-    database:"atlan"
+    user: "postgres",
+    password: "Satvik26#",
+    port: 5432,
+    database: "atlan"
 });
 
 module.exports = pool;
